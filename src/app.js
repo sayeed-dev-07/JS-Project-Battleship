28cc8620af6d0e9aa1d433f75c3ea0d0e570a597
sayeed <expsayeedshorif@gmail.com>
2025-05-21 10:24:33 +0600

    working on receive attack funciton

--- src/app.js
+++ src/app.js
@@ -1,27 +1,77 @@
 /* eslint-disable indent */
 
 export class Ship {
-    constructor(length) {
+    constructor(length, name) {
+        this.name = name;
         this.length = length;
         this.hit = 0;
+
     }
-    hits(){
+    hits() {
         if (!this.isSunk()) {
-            this.hit ++;
+            this.hit++;
         }
     }
 
-    isSunk(){
+    isSunk() {
         return this.hit >= this.length;
     }
 }
 
-export class GameBoard{
-    constructor(){
-        this.board = new Array(100).fill(null) ;
+export class GameBoard {
+    constructor() {
+        this.board = new Array(100).fill(null);
         this.ships = [];
+        this.missShot = [];
     }
-    placeShip(){
-        
+    placeShip(name, start, length, direction = 'hrz') {
+        const position = [];
+        const ship = new Ship(length, name);
+        for (let i = 0; i < length; i++) {
+            let x = start.x;
+            let y = start.y;
+
+            if (direction === 'hrz') {
+                x = start.x + i;
+            } else if (direction === 'vrtx') {
+                y = start.y + i;
+            } else {
+                return 'invalid direction';
+            }
+
+            if (x >= 10 || y >= 10) {
+                return 'Index out of bounds';
+            }
+            const index = y * 10 + x;
+
+            if (this.board[index] !== null) {
+                return 'overlap';
+            }
+
+            position.push(index);
+
+        }
+        position.forEach(index => {
+            this.board[index] = ship;
+        });
+        this.ships.push({ ship, position });
+        return true;
+    }
+    receiveAttack(obj) {
+        let x = obj.x;
+        let y = obj.y;
+        if (x >= 10 || y >= 10) {
+            return 'Index out of bounds';
+        }
+        const index = y * 10 + x;
+        if (this.board[index] === null) {
+            this.missShot.push(index);
+            this.board[index] = 'miss';
+            return false;
+        }else{
+            let ship = this.board[index];
+            ship.hit();
+        }
+
     }
 }
--- src/ship.test.js
+++ src/ship.test.js
@@ -1,15 +1,17 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-multiple-empty-lines */
 /* eslint-disable no-undef */
 /* eslint-disable quotes */
-import { Ship , GameBoard} from "./app.js";
+import { Ship, GameBoard } from "./app.js";
 
 describe('Ship class', () => {
   test('initial ship is not sunk', () => {
-    const ship = new Ship(3);
+    const ship = new Ship(3, 'rocket');
     expect(ship.isSunk()).toBe(false);
   });
 
   test('ship records hits correctly', () => {
-    const ship = new Ship(2);
+    const ship = new Ship(2, 'rocket');
     ship.hits();
     expect(ship.isSunk()).toBe(false);
     ship.hits();
@@ -17,7 +19,7 @@ describe('Ship class', () => {
   });
 
   test('ship cannot be over-hit', () => {
-    const ship = new Ship(1);
+    const ship = new Ship(1, 'rocket');
     ship.hits(); // hitss = 1
     ship.hits(); // should do nothing
     ship.hits(); // still nothing
@@ -25,7 +27,58 @@ describe('Ship class', () => {
   });
 
   test('ship with length 0 is instantly sunk (edge case)', () => {
-    const ship = new Ship(0);
+    const ship = new Ship(0, 'rocket');
     expect(ship.isSunk()).toBe(true);
   });
+});
+
+describe('GameBoard Class', () => {
+
+  test('Places a ship incorrectly - 1', () => {
+    const board = new GameBoard();
+    const ship = board.placeShip('rocket', { x: 12, y: 2 }, 3, 'hrz');
+
+    expect(ship).toBe('Index out of bounds');
+  });
+
+  test('Places a ship incorrectly - 2', () => {
+    const board = new GameBoard();
+    const ship = board.placeShip('rocket', { x: 12, y: 2 }, 3, 'horizontal');
+    expect(ship).toBe('invalid direction');
+  });
+
+  test('Places a ship on another one', () => {
+    const board = new GameBoard();
+    const ship = board.placeShip('rocket', { x: 1, y: 2 }, 3, 'hrz');
+    const ship2 = board.placeShip('rocket2', { x: 3, y: 2 }, 3, 'hrz');
+    expect(ship2).toBe('overlap');
+  });
+
+  test('board equiped index not be null', () => {
+    const board = new GameBoard();
+    const ship = board.placeShip('rocket', { x: 1, y: 2 }, 3, 'hrz');
+    const ship2 = board.placeShip('rocket2', { x: 5, y: 2 }, 3, 'hrz');
+    expect(board.board[27]).toBeNull();
+  });
+});
+
+describe('Receive Attack Method Check : ', () => {
+
+  test('out of bounds index attack - 1', () => {
+
+    const board = new GameBoard();
+    const ship = board.placeShip('rocket', { x: 1, y: 2 }, 3, 'hrz');
+    const ship2 = board.placeShip('rocket2', { x: 5, y: 2 }, 3, 'hrz');
+    expect(board.receiveAttack({ x: 12, y: 2 })).toBe('Index out of bounds');
+    expect(board.receiveAttack({ x: 2, y: 3 })).toBe(false);
+  });
+
+  test('miss shot  - 1', () => {
+
+    const board = new GameBoard();
+    const ship = board.placeShip('rocket', { x: 1, y: 2 }, 3, 'hrz');
+    const ship2 = board.placeShip('rocket2', { x: 5, y: 2 }, 3, 'hrz');
+    expect(board.receiveAttack({ x: 2, y: 3 })).toBe(false);
+  });
+
 });
