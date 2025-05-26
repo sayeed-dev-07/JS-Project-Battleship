267343b8cbdaf4e6cc0ec3eb018445798eb1e193
sayeed <expsayeedshorif@gmail.com>
2025-05-26 18:08:44 +0600

    almost finished the ui now i will finish the computer attack logic first

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
+/* eslint-disable no-multiple-empty-lines */
+/* eslint-disable indent */
 /* eslint-disable no-unused-vars */
 import style from './style.css';
-console.log('hello wrorld');
+import { RealPlayer, Computer } from './app.js';
+
+let player = new RealPlayer();
+let computer = new Computer();
+
+const grid1 = document.querySelector('.grid-1');
+const grid2 = document.querySelector('.grid-2');
+
+function gridCreate(obj){
+    player.board.board.forEach(elem => {
+    const element = document.createElement('div');
+    element.classList.add('item');
+    obj.appendChild(element);
+});
+}
+
+gridCreate(grid1);
+gridCreate(grid2);
+
+
